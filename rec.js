1186d808c2e7fd37f1e832e356e78aeb9006a450
AMN-47 <akhilnagar57@gmail.com>
2026-03-24 19:18:30 +0200

    Added for loop to do fibonaci sequence by iteration, still empty, to add code

--- rec.js
+++ rec.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node 
 console.log("hello world")
+
+//1st Assignment - Fibs  - Using iteration
+const fibs = (number) => {
+    let fibArray = [];
+
+    for (let i = 0; i < number; i++) {
+        
+    }
+};
