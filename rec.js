c8411c4724343756d158b0aed81de80ff1fea8ee
AMN-47 <akhilnagar57@gmail.com>
2026-03-25 16:28:22 +0200

    added if statment for iteration in for loop

--- rec.js
+++ rec.js
@@ -6,6 +6,12 @@ const fibs = (number) => {
     let fibArray = [];
 
     for (let i = 0; i < number; i++) {
-        
-    }
+        if ((fibArray[0] == null) || (fibArray[1] == null)) {
+            fibArray.push(i);
+        } else {
+            let fibNumber = fibArray[fibArray.length -1] + fibArray[fibArray.length -2];
+            fibArray.push(fibNumber);
+        };
+    };
+    console.log("Iteration", fibArray);
 };
